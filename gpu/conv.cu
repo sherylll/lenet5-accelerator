cc63556bee44799ecbdbd3fd435c77a75ffff080
sherylll <sherylsun14@gmail.com>
2019-07-03 18:03:41 +0900

    some more optimizations

--- gpu/conv.cu
+++ gpu/conv.cu
@@ -10,11 +10,12 @@ __global__ void conv_2d_2(
 {   
     int oh= blockIdx.y * blockDim.y + threadIdx.y;
     int ow= blockIdx.x * blockDim.x + threadIdx.x;
-    if (oh>=OUT_HEIGHT_1 || ow>=OUT_WIDTH_1)
+    if (oh>=IN_HEIGHT_3-FILT_HEIGHT+1 || ow>=IN_WIDTH_3-FILT_WIDTH+1)
         return;
+    int offset = (oh * OUT_WIDTH_3 + ow)*N_FILT_3;
     for (int ff = 0; ff < N_FILT_3; ff++)
     {
-        float temp = 0;
+        float temp = biases[ff];
         for (int cc = 0; cc < N_CHAN_3; cc++)
         {
             for (int fh = 0; fh < FILT_HEIGHT; fh++)
@@ -23,14 +24,12 @@ __global__ void conv_2d_2(
                 {
                     int index_weight = fh * FILT_WIDTH * N_CHAN_3 * N_FILT_3 + fw * N_CHAN_3 * N_FILT_3 + cc * N_FILT_3 + ff;
                     // assuming there is no padding
-                    if ((oh + fh) < IN_HEIGHT_3 && (ow + fw) < IN_WIDTH_3)
+                    //if ((oh + fh) < IN_HEIGHT_3 && (ow + fw) < IN_WIDTH_3)
                         temp += data[((oh + fh) * IN_WIDTH_3 + (ow + fw)) * N_CHAN_3 + cc] * weights[index_weight];
         
                 } //end mult loop
             }     //end channel loop
-
         } //end filter width loop
-        float res_ = temp + biases[ff];
-        res[(oh * OUT_WIDTH_3 + ow) * N_FILT_3 + ff] = (res_ > 0)?res_:0;
+        res[offset + ff] = (temp > 0)?temp:0;
     }     //end filter height loop
 } //end conv2d
